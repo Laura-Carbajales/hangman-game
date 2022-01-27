7eca2420decaae6b5879c764721239ddf8511120
Laura Carbajales <laura.carbajales@gmail.com>
2022-01-27 18:59:43 +0100

    Create components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import '../styles/App.scss';
 import { useState, useEffect } from 'react';
 import callToApi from '../services/api.js';
+import Header from './Header';
+import Dummy from './Dummy';
+import ErrorLetters from './ErrorLetters';
+import SolutionLetters from './SolutionLetters';
+import Form from './Form';
 
 const App = () => {
   //Estado palabra a adivinar
@@ -40,82 +45,17 @@ const App = () => {
     }
   };
 
-  const renderSolutionLetters = () => {
-    const wordLetters = word.split('');
-    return wordLetters.map((letter, index) => {
-      if (goodLetters.findIndex((currentLetter) => letter === currentLetter) !== -1) {
-        return (
-          <li key={index} className='letter'>
-            {letter}
-          </li>
-        );
-      } else {
-        return <li key={index} className='letter'></li>;
-      }
-    });
-  };
-
-  const renderWrongLetters = () => {
-    return wrongLetters.map((letter, index) => {
-      if (wrongLetters.findIndex((currentLetter) => letter === currentLetter) !== -1) {
-        return (
-          <li key={index} className='letter'>
-            {letter}
-          </li>
-        );
-      } else {
-        return <li key={index} className='letter'></li>;
-      }
-    });
-  };
-
   return (
     <div>
       <div className='page'>
-        <header>
-          <h1 className='header__title'>Juego del ahorcado</h1>
-        </header>
+        <Header title='Juego del ahorcado' />
         <main className='main'>
           <section>
-            <div className='solution'>
-              <h2 className='title'>Solución:</h2>
-              <ul className='letters'>{renderSolutionLetters()}</ul>
-            </div>
-            <div className='error'>
-              <h2 className='title'>Letras falladas:</h2>
-              <ul className='letters'>{renderWrongLetters()}</ul>
-            </div>
-            <form className='form'>
-              <label className='title' htmlFor='last-letter'>
-                Escribe una letra:
-              </label>
-              <input
-                autoComplete='off'
-                className='form__input'
-                maxLength='1'
-                type='text'
-                name='last-letter'
-                id='last-letter'
-                value={lastLetter}
-                onChange={handleLastLetter}
-              />
-            </form>
+            <SolutionLetters word={word} goodLetters={goodLetters} />
+            <ErrorLetters wrongLetters={wrongLetters} />
+            <Form handleLastLetter={handleLastLetter} />
           </section>
-          <section className={`dummy error-${wrongLetters.length}`}>
-            <span className='error-13 eye'></span>
-            <span className='error-12 eye'></span>
-            <span className='error-11 line'></span>
-            <span className='error-10 line'></span>
-            <span className='error-9 line'></span>
-            <span className='error-8 line'></span>
-            <span className='error-7 line'></span>
-            <span className='error-6 head'></span>
-            <span className='error-5 line'></span>
-            <span className='error-4 line'></span>
-            <span className='error-3 line'></span>
-            <span className='error-2 line'></span>
-            <span className='error-1 line'></span>
-          </section>
+          <Dummy length={wrongLetters.length} />
         </main>
       </div>
     </div>
---
+++ src/components/ErrorLetters.js
@@ -0,0 +1,26 @@
+import '../styles/layout/Letters.scss';
+
+const ErrorLetters = (props) => {
+  const renderWrongLetters = () => {
+    return props.wrongLetters.map((letter, index) => {
+      if (props.wrongLetters.findIndex((currentLetter) => letter === currentLetter) !== -1) {
+        return (
+          <li key={index} className='letter'>
+            {letter}
+          </li>
+        );
+      } else {
+        return <li key={index} className='letter'></li>;
+      }
+    });
+  };
+
+  return (
+    <div className='error'>
+      <h2 className='title'>Letras falladas:</h2>
+      <ul className='letters'>{renderWrongLetters()}</ul>
+    </div>
+  );
+};
+
+export default ErrorLetters;
